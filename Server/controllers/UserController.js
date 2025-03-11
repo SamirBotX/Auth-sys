f65b29e85f75421e69fef6d888fffcc192b7787e
thedebug7 <thedebug7@gmail.com>
2025-03-11 18:31:19 +0200

    add signup

--- Server/controllers/UserController.js
+++ Server/controllers/UserController.js
@@ -1,4 +1,4 @@
-import { AllUser} from '../model/UserModel.js';
+import { AllUser, CreateUser, FindByEmail} from '../model/UserModel.js';
 
 
 export const GetAllUsers = async (req, res) => {
@@ -11,4 +11,30 @@ export const GetAllUsers = async (req, res) => {
         // Send an appropriate error response
         res.status(500).json({message: 'Internal Server Error: unable to fetch users.'})
     }
+}
+
+export const Signup = async (req, res) => {
+    
+    try {
+        const {username, email, password} = req.body
+        const CheckExistsUser = await FindByEmail(email);
+        if(CheckExistsUser){
+            res.status(400).json({message: 'User already exists.'})
+        }
+
+        // create a new user
+        const NewUser = await CreateUser(username, email, password);
+        res.status(200).json({
+            message: 'User created successfully',
+            Username: NewUser.username,
+            Email: NewUser.email,
+        });
+    } catch (error) {
+        console.error(`Error during creating user: ${error.message}`);
+        res.status(500).json({
+            message: `Internal Server Error: unable to create user: ${error.message}`
+
+        });
+    }
+
 }
--- Server/model/UserModel.js
+++ Server/model/UserModel.js
@@ -6,7 +6,14 @@ export const AllUser =  async () => {
     return users.rows;    
 }
 
-const CreateUser = async (username, email, password) => {
+export const CreateUser = async (username, email, password) => {
     const user = await pool.query(`INSERT INTO users (username, email, password) VALUES ($1, $2, $3) RETURNING userid, username, email`, [username, email, password]);
     return user.rows[0];
 }
+
+export const FindByEmail = async(email) => {
+    const user = await pool.query(`SELECT * FROM users WHERE email = $1`, [email]);
+    return user.rows[0];
+}
+
+
